4030013e6ff43f625795eb61071fca0532c2c2c0
Keisuke ANDO <ando@maslab.aitech.ac.jp>
2025-08-26 14:21:53 +0900

    :sparkles: 自作したシナリオを指定できる機能を追加

--- rcrs-server/launch-server.sh
+++ rcrs-server/launch-server.sh
@@ -3,9 +3,36 @@ set -e
 
 cd /app/rcrs-server/scripts
 
+if [ -z "$MAP_NAME" ]; then
+  echo "[ERROR] MAP_NAME environment variable is not set." >&2
+  exit 1
+fi
+
+CUSTOM_MAP_DIR="../custom-maps/${MAP_NAME}"
+STANDARD_MAP_DIR="../maps/${MAP_NAME}"
+MAP_DIR_TO_USE=""
+
+# Use custom map if it exists
+if [ -d "$CUSTOM_MAP_DIR" ]; then
+  MAP_DIR_TO_USE="$CUSTOM_MAP_DIR"
+  echo ">>> Using custom map: '$MAP_NAME'"
+
+# Use standard map if custom map doesn't exist
+elif [ -d "$STANDARD_MAP_DIR" ]; then
+  MAP_DIR_TO_USE="$STANDARD_MAP_DIR"
+  echo ">>> Using standard map: '$MAP_NAME'"
+
+# Specified map not found
+else
+  echo "[ERROR] Map not found: '$MAP_NAME'" >&2
+  exit 1
+fi
+
+MAP_ARGS="-m ${MAP_DIR_TO_USE}/map -c ${MAP_DIR_TO_USE}/config"
+
 case "$RUN_TYPE" in
   precompute)
-    ./start-precompute.sh -m ../maps/${MAP_NAME}/map -c ../maps/${MAP_NAME}/config -g &
+    ./start-precompute.sh $MAP_ARGS -g &
     SERVER_PID=$!
     echo "Server started with PID $SERVER_PID"
     echo "Waiting for precomputation to finish..."
@@ -25,7 +52,7 @@ case "$RUN_TYPE" in
     exit 0
     ;;
   comprun)
-    ./start-comprun.sh -m ../maps/${MAP_NAME}/map -c ../maps/${MAP_NAME}/config -g
+    ./start-comprun.sh $MAP_ARGS -g
     echo "Comprun finished. Creating done file..."
     touch /app/shared/comprun.done
     ;;
